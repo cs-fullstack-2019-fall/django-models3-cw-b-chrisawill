acffb9e1ce08c0dc1384c00458dcaa9ff674c305
tdude0175 <tdude0175@gmail.com>
2019-09-25 15:55:36 -0500

    Graded 2019-09-25

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # django-models3-cw_b
-
+#### You have a functional model and you added an element to the database. Your routes do not work correctly. Please Refer to comments inside views.py 2/5 Score: 1/5
 ### Exercise 1
 Create a Book model with name, pageNumber, genre, and publishDate attributes. Create 2 entries using 2 different methods (admin site and class construtor using an endpoint).
 
--- projectB/projectBapp/views.py
+++ projectB/projectBapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# You need to import Books from models
 from django.http import HttpResponse
 def library(request):
     return HttpResponse('hello')
@@ -11,19 +13,19 @@ def library(request):
 
 def allbooks(request):
     tempstrings = ""
-    booklist = books.objects.all()
+    booklist = books.objects.all()  #You need to matchs "books" to your model or what you import it as
     for eachbook in booklist:
-        tempstrings = tempstrings + eachbook + "."
+        tempstrings = tempstrings + eachbook + "."   #you cannot combine a string with data from a model. You need to turn eachbook into a string and then you need to ask for a piece of information like name or page number. Then it will return the book as a Visible string information
     return HttpResponse(tempstrings)
 
 
-def addBook():
-    getallbooksfromdatabase = Books.objects.all
+def addBook():   #you need to define request
+    getallbooksfromdatabase = Books.objects.all   #you need to add parenthesis to the end of "all"
     for eachbook in getallbooksfromdatabase:
         print(eachbook)
     return HttpResponse("All books")
-def fictionbooks(request)
-    getallbooksfromdatabase = Books.objects.all
+def fictionbooks(request)   #you need to have a colon at the end of the parenthesis
+    getallbooksfromdatabase = Books.objects.all  # you need parenthesis at the end of "all"
     for eachbook in getallbooksfromdatabase:
         if(eachbook.works == "fiction"):
             print(eachbook)
